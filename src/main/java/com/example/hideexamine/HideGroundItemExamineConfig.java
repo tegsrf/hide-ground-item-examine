bd994cc599be26a6366407ea9c6aef07f48b20d5
tegsrf <tegsrf@users.noreply.github.com>
2026-03-13 21:42:29 +0000

    Add Hide Ground Item Examine plugin

---
+++ src/main/java/com/example/hideexamine/HideGroundItemExamineConfig.java
@@ -0,0 +1,9 @@
+package com.example.hideexamine;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("hidegrounditemexamine")
+public interface HideGroundItemExamineConfig extends Config
+{
+}
